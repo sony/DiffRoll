b9e8b8a6b6353d91ee422a1f0fc24f8f10e3ead5
Kin Wai Cheuk <kinwai.cheuk@sony.com>
2022-07-12 09:49:41 +0900

    added a new model and task for spec condition

--- model/unet.py
+++ model/unet.py
@@ -6,10 +6,28 @@ import torch.nn.functional as F
 from inspect import isfunction
 from functools import partial
 import math
-from task.diffusion import RollDiffusion
+from task.diffusion import RollDiffusion, SpecRollDiffusion
+import torchaudio
+EPSILON = 1e-6
 
 def exists(x):
     return x is not None
+
+def align_timesteps(x1, x2):
+    """
+    x1 and x2 must be of the shape (B, C, T, F)
+    Then this function will discard the extra timestep
+    in either x1 or x2 such that they match in T dimension.
+    """
+    
+    T1 = x1.shape[2]
+    T2 = x2.shape[2]
+    
+    Tmin = min(T1, T2)
+    x1 = x1[:, :, :Tmin, :]
+    x2 = x2[:, :, :Tmin, :]
+    
+    return x1, x2
 
 def default(val, d):
     if exists(val):
@@ -299,6 +317,243 @@ class Unet(RollDiffusion):
         x = self.final_conv(x)
 
         return x
+    
+    
+class SpecConvNextBlock(nn.Module):
+    """https://arxiv.org/abs/2201.03545"""
+
+    def __init__(self, dim, dim_out, *, time_emb_dim=None, mult=2, norm=True):
+        super().__init__()
+        self.mlp = (
+            nn.Sequential(nn.GELU(), nn.Linear(time_emb_dim, dim))
+            if exists(time_emb_dim)
+            else None
+        )
+
+        self.ds_conv = nn.Conv2d(dim, dim, 7, padding=3, groups=dim)
+        
+        self.spec_ds_conv = nn.Conv2d(dim, dim, 7, padding=3, groups=dim)
+
+        self.net = nn.Sequential(
+            nn.GroupNorm(1, dim) if norm else nn.Identity(),
+            nn.Conv2d(dim, dim_out * mult, 3, padding=1),
+            nn.GELU(),
+            nn.GroupNorm(1, dim_out * mult),
+            nn.Conv2d(dim_out * mult, dim_out, 3, padding=1),
+        )
+        
+        self.spec_net = nn.Sequential(
+            nn.GroupNorm(1, dim) if norm else nn.Identity(),
+            nn.Conv2d(dim, dim_out * mult, 3, padding=1),
+            nn.GELU(),
+            nn.GroupNorm(1, dim_out * mult),
+            nn.Conv2d(dim_out * mult, dim_out, 3, padding=1),
+        )        
+
+        self.res_conv = nn.Conv2d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
+
+    def forward(self, x, spec, time_emb=None):
+        h = self.ds_conv(x)
+        spec_h = self.spec_ds_conv(spec)
+        if exists(self.mlp) and exists(time_emb):
+            assert exists(time_emb), "time embedding must be passed in"
+            condition = self.mlp(time_emb)
+            h = h + spec_h + rearrange(condition, "b c -> b c 1 1")
+
+        h = self.net(h)
+        spec_h = self.spec_net(spec_h)
+        return h + self.res_conv(x), spec_h
+    
+    
+class SpecConvNextBlockUp(nn.Module):
+    """https://arxiv.org/abs/2201.03545"""
+
+    def __init__(self, dim, dim_out, *, time_emb_dim=None, mult=2, norm=True):
+        super().__init__()
+        self.mlp = (
+            nn.Sequential(nn.GELU(), nn.Linear(time_emb_dim, dim))
+            if exists(time_emb_dim)
+            else None
+        )
+    
+        self.ds_conv = nn.Conv2d(dim, dim, 7, padding=3, groups=dim)
+        self.spec_ds_conv = nn.Conv2d(dim//3, dim, 7, padding=3, groups=1)
+
+        self.net = nn.Sequential(
+            nn.GroupNorm(1, dim) if norm else nn.Identity(),
+            nn.Conv2d(dim, dim_out * mult, 3, padding=1),
+            nn.GELU(),
+            nn.GroupNorm(1, dim_out * mult),
+            nn.Conv2d(dim_out * mult, dim_out, 3, padding=1),
+        )
+        
+        self.spec_net = nn.Sequential(
+            nn.GroupNorm(1, dim) if norm else nn.Identity(),
+            nn.Conv2d(dim, dim_out * mult, 3, padding=1),
+            nn.GELU(),
+            nn.GroupNorm(1, dim_out * mult),
+            nn.Conv2d(dim_out * mult, dim_out, 3, padding=1),
+        )        
+
+        self.res_conv = nn.Conv2d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
+
+    def forward(self, x, spec, time_emb=None):
+        h = self.ds_conv(x)
+        spec_h = self.spec_ds_conv(spec)
+        if exists(self.mlp) and exists(time_emb):
+            assert exists(time_emb), "time embedding must be passed in"
+            condition = self.mlp(time_emb)
+            h = h + spec_h + rearrange(condition, "b c -> b c 1 1")
+
+        h = self.net(h)
+        spec_h = self.spec_net(spec_h)
+        return h + self.res_conv(x), spec_h    
+    
+class SpecUnet(SpecRollDiffusion):
+    # Unet conditioned on spectrogram
+    def __init__(
+        self,
+        dim,
+        init_dim=None,
+        out_dim=None,
+        dim_mults=(1, 2, 4, 8),
+        channels=3,
+        with_time_emb=True,
+        resnet_block_groups=8,
+        use_convnext=True,
+        convnext_mult=2,
+        spec_args={},
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        init_dim = default(init_dim, dim // 3 * 2)
+        self.init_conv = nn.Conv2d(channels, init_dim, 7, padding=3)
+        
+        # Initial layers for spectrograms
+        self.spec_init_conv = nn.Conv2d(channels, init_dim, 7, padding=3)
+        self.spec_init_fc = nn.Linear(spec_args.n_mels, 88)
+        
+        self.mel_layer = torchaudio.transforms.MelSpectrogram(**spec_args)
+
+        dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        if use_convnext:
+            block_klass = partial(SpecConvNextBlock, mult=convnext_mult)
+            up_block_klass = partial(SpecConvNextBlockUp, mult=convnext_mult)
+        else:
+            block_klass = partial(ResnetBlock, groups=resnet_block_groups)
+
+        # time embeddings
+        if with_time_emb:
+            time_dim = dim * 4
+            self.time_mlp = nn.Sequential(
+                SinusoidalPositionEmbeddings(dim),
+                nn.Linear(dim, time_dim),
+                nn.GELU(),
+                nn.Linear(time_dim, time_dim),
+            )
+        else:
+            time_dim = None
+            self.time_mlp = None
+
+        # layers
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        
+        self.spec_downs = nn.ModuleList([])
+        self.spec_ups = nn.ModuleList([])
+        
+        num_resolutions = len(in_out)
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(
+                nn.ModuleList(
+                    [
+                        block_klass(dim_in, dim_out, time_emb_dim=time_dim),
+                        block_klass(dim_out, dim_out, time_emb_dim=time_dim),
+                        Residual(PreNorm(dim_out, LinearAttention(dim_out))),
+                        Downsample(dim_out) if not is_last else nn.Identity(),
+                        Downsample(dim_out) if not is_last else nn.Identity(),
+                    ]
+                )
+            )     
+
+        mid_dim = dims[-1]
+        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_attn = Residual(PreNorm(mid_dim, Attention(mid_dim)))
+        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.ups.append(
+                nn.ModuleList(
+                    [
+                        up_block_klass(dim_out * 3, dim_in, time_emb_dim=time_dim),
+                        block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                        Residual(PreNorm(dim_in, LinearAttention(dim_in))),
+                        Upsample(dim_in) if not is_last else nn.Identity(),
+                        Upsample(dim_in) if not is_last else nn.Identity(),
+                    ]
+                )
+            )
+
+        out_dim = default(out_dim, channels)
+        self.final_block = block_klass(dim, dim)
+        self.final_conv = nn.Conv2d(dim, out_dim, 1)
+        
+
+
+    def forward(self, x, waveform, time):
+        spec = self.mel_layer(waveform)
+        spec = spec.transpose(1,2) # (B, T, n_mels)
+        spec = torch.log(spec+EPSILON)
+        spec = spec.unsqueeze(1)
+        
+        spec, x = align_timesteps(spec, x)
+        
+        spec = self.spec_init_conv(spec)
+        spec = self.spec_init_fc(spec)
+        
+        # x = (B, 1, dim, dim)       
+        x = self.init_conv(x) # (B, 18, dim, dim)
+        t = self.time_mlp(time) if exists(self.time_mlp) else None # (B, dim*4)
+        h = []
+
+        # downsample
+        counter = 0
+        for block1, block2, attn, downsample, spec_downsample in self.downs:
+            x, spec = block1(x, spec, t)
+            x, spec = block2(x, spec, t)
+            x = attn(x)
+            h.append([x, spec])
+            x = downsample(x)
+            spec = downsample(spec)
+            counter += 1 
+        # bottleneck
+        x, spec = self.mid_block1(x, spec, t)
+        x = self.mid_attn(x)
+        x, spec = self.mid_block2(x, spec, t)
+        
+
+        # upsample
+        for block1, block2, attn, upsample, spec_upsample in self.ups:       
+            x = torch.cat((x, *h.pop()), dim=1)
+            x, spec = block1(x, spec, t)
+            x, spec = block2(x, spec, t)
+            x = attn(x)
+            x = upsample(x)
+            spec = spec_upsample(spec)
+            
+        x, spec = self.final_block(x, spec)
+        x = self.final_conv(x)
+
+        return x
+    
 
 def cosine_beta_schedule(timesteps, s=0.008):
     """
--- task/diffusion.py
+++ task/diffusion.py
@@ -189,3 +189,158 @@ class RollDiffusion(pl.LightningModule):
 
 #         return [optimizer], [{"scheduler":scheduler, "interval": "step"}]
         return [optimizer]
+
+
+
+
+
+
+
+class SpecRollDiffusion(pl.LightningModule):
+    def __init__(self,
+                 lr,
+                 timesteps,
+                 loss_type
+                ):
+        super().__init__()
+        
+        self.save_hyperparameters()
+        
+        # define beta schedule
+        # beta is variance
+        self.betas = linear_beta_schedule(timesteps=timesteps)
+
+        # define alphas 
+        alphas = 1. - self.betas
+        alphas_cumprod = torch.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+        self.sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        self.posterior_variance = self.betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+
+    def training_step(self, batch, batch_idx):
+        batch_size = batch["frame"].shape[0]
+        roll = batch["frame"].unsqueeze(1)  
+        waveform = batch["audio"]
+        device = roll.device
+        # Algorithm 1 line 3: sample t uniformally for every example in the batch
+        t = torch.randint(0, self.hparams.timesteps, (batch_size,), device=device).long()
+
+        loss = self.p_losses(roll, waveform, t, self.sqrt_alphas_cumprod, self.sqrt_one_minus_alphas_cumprod, loss_type=self.hparams.loss_type)
+        self.log("Train/loss", loss)            
+      
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        batch_size = batch["frame"].shape[0]
+        roll = batch["frame"].unsqueeze(1)
+        waveform = batch["audio"]
+        device = roll.device
+        # Algorithm 1 line 3: sample t uniformally for every example in the batch
+        t = torch.randint(0, self.hparams.timesteps, (batch_size,), device=device).long()
+
+        loss = self.p_losses(roll, waveform, t, self.sqrt_alphas_cumprod, self.sqrt_one_minus_alphas_cumprod, loss_type=self.hparams.loss_type)
+        self.log("Val/loss", loss)
+        
+    def test_step(self, batch, batch_idx):
+        batch_size = batch["frame"].shape[0]
+        batch = batch["audio"].unsqueeze(1)
+        device = roll.device
+        
+        
+        # Algorithm 1 line 3: sample t uniformally for every example in the batch
+        t = torch.randint(0, self.hparams.timesteps, (batch_size,), device=device).long()
+
+        loss = self.p_losses(roll, waveform, t, self.sqrt_alphas_cumprod, self.sqrt_one_minus_alphas_cumprod, loss_type=self.hparams.loss_type)
+        self.log("Test/loss", loss)        
+        
+    def predict_step(self, batch, batch_idx):
+        # inference code
+        # Unwrapping TensorDataset (list)
+        # It is a pure noise
+        img = batch[0]
+        b = img.shape[0] # extracting batchsize
+        device=img.device
+        imgs = []
+        for i in tqdm(reversed(range(0, self.hparams.timesteps)), desc='sampling loop time step', total=self.hparams.timesteps):
+            img = self.p_sample(
+                           img,
+                           torch.full(
+                               (b,),
+                               i,
+                               device=device,
+                               dtype=torch.long),
+                           i)
+            img_npy = img.cpu().numpy()
+            
+            if (i+1)%10==0:
+                for idx, j in enumerate(img_npy):
+                    # j (1, T, F)
+                    fig, ax = plt.subplots(1,1)
+                    ax.imshow(j[0].T, aspect='auto', origin='lower')
+                    self.logger.experiment.add_figure(
+                        f"sample_{idx}",
+                        fig,
+                        global_step=self.hparams.timesteps-i)
+                    # self.hparams.timesteps-i is used because slide bar won't show
+                    # if global step starts from self.hparams.timesteps
+            imgs.append(img_npy)
+        torch.save(imgs, 'imgs.pt')
+    
+    def p_losses(self, x_start, waveform, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, noise=None, loss_type="l1"):
+        if noise is None:
+            noise = torch.randn_like(x_start)
+
+        x_noisy = q_sample(x_start=x_start, t=t, sqrt_alphas_cumprod=sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod=sqrt_one_minus_alphas_cumprod, noise=noise)
+        predicted_noise = self(x_noisy, waveform, t)
+
+        if loss_type == 'l1':
+            loss = F.l1_loss(noise, predicted_noise)
+        elif loss_type == 'l2':
+            loss = F.mse_loss(noise, predicted_noise)
+        elif loss_type == "huber":
+            loss = F.smooth_l1_loss(noise, predicted_noise)
+        else:
+            raise NotImplementedError()
+
+        return loss
+    
+    def p_sample(self, x, t, t_index):
+        # x is Guassian noise
+        
+        betas_t = extract(self.betas, t, x.shape)
+        sqrt_one_minus_alphas_cumprod_t = extract(
+            self.sqrt_one_minus_alphas_cumprod, t, x.shape
+        )
+        sqrt_recip_alphas_t = extract(self.sqrt_recip_alphas, t, x.shape)
+
+        # Equation 11 in the paper
+        # Use our model (noise predictor) to predict the mean
+        model_mean = sqrt_recip_alphas_t * (
+            x - betas_t * self(x, t) / sqrt_one_minus_alphas_cumprod_t
+        )
+
+        if t_index == 0:
+            return model_mean
+        else:
+            posterior_variance_t = extract(self.posterior_variance, t, x.shape)
+            noise = torch.randn_like(x)
+            # Algorithm 2 line 4:
+            return model_mean + torch.sqrt(posterior_variance_t) * noise             
+ 
+    def configure_optimizers(self):
+
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+#         scheduler = TriStageLRSchedule(optimizer,
+#                                        [1e-8, self.hparams.lr, 1e-8],
+#                                        [0.2,0.6,0.2],
+#                                        max_update=len(self.train_dataloader.dataloader)*self.trainer.max_epochs)   
+#         scheduler = MultiStepLR(optimizer, [1,3,5,7,9], gamma=0.1, last_epoch=-1, verbose=False)
+
+#         return [optimizer], [{"scheduler":scheduler, "interval": "step"}]
+        return [optimizer]
