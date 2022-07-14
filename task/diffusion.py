a4636ddeeab314e951633d9c7c22405fe4c82aa7
Kin Wai Cheuk <kinwai.cheuk@sony.com>
2022-07-14 14:40:05 +0900

    refactored test_step

--- task/diffusion.py
+++ task/diffusion.py
@@ -13,6 +13,11 @@ def linear_beta_schedule(timesteps):
     return torch.linspace(beta_start, beta_end, timesteps)
 
 def q_sample(x_start, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, noise=None):
+    """
+    x_start: x0
+    t: timestep information
+    """    
+    
     # sqrt_alphas is mean of the Gaussian N()    
     sqrt_alphas_cumprod_t = extract(sqrt_alphas_cumprod, t, x_start.shape) # extract the value of \bar{\alpha} at time=t
     # sqrt_alphas is variance of the Gaussian N()
@@ -23,6 +28,24 @@ def q_sample(x_start, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, noi
 
     # scale down the input, and scale up the noise as time increases?
     return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
+
+
+def extract_x0(x_t, epsilon, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod):
+    """
+    x_t: The output from q_sample
+    epsilon: The noise predicted from the model
+    t: timestep information
+    """
+    
+    # sqrt_alphas is mean of the Gaussian N()    
+    sqrt_alphas_cumprod_t = extract(sqrt_alphas_cumprod, t, x_t.shape) # extract the value of \bar{\alpha} at time=t
+    # sqrt_alphas is variance of the Gaussian N()
+    sqrt_one_minus_alphas_cumprod_t = extract(
+        sqrt_one_minus_alphas_cumprod, t, x_t.shape
+    )
+
+    # obtaining x0 based on the inverse of eq.4 of DDPM paper
+    return (x_t - sqrt_one_minus_alphas_cumprod_t * epsilon) / sqrt_alphas_cumprod_t
 
 
 def extract(a, t, x_shape):
@@ -218,7 +241,7 @@ class SpecRollDiffusion(pl.LightningModule):
         self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
 
         # calculations for posterior q(x_{t-1} | x_t, x_0)
-        self.posterior_variance = self.betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+        self.posterior_variance = self.betas * (1. - alphas_cumprod_prev) / (1- alphas_cumprod)
 
     def training_step(self, batch, batch_idx):
         batch_size = batch["frame"].shape[0]
@@ -245,6 +268,23 @@ class SpecRollDiffusion(pl.LightningModule):
         self.log("Val/loss", loss)
         
     def test_step(self, batch, batch_idx):
+        losses, tensors = self.step(batch)
+        self.log("Test/diffusion_loss", losses['diffusion_loss'])
+        self.log("Test/amt_loss", losses['amt_loss'])
+        
+        if batch_idx == 0:
+            self.visualize_figure(tensors['pred_roll'], 'Test/pred_roll', batch_idx)
+            self.visualize_figure(tensors['label_roll'], 'Test/label_roll', batch_idx)
+        
+    def visualize_figure(self, tensors, tag, batch_idx):
+        fig, ax = plt.subplots(2,2)
+        for idx in range(4): # visualize only 4 piano rolls
+            # roll_pred (1, T, F)
+            ax.flatten()[idx].imshow(tensors[idx][0].T.cpu(), aspect='auto', origin='lower')
+        self.logger.experiment.add_figure(f"{tag}{idx}", fig)
+        plt.close()
+        
+    def step(self, batch):
         batch_size = batch["frame"].shape[0]
         roll = batch["frame"].unsqueeze(1)
         waveform = batch["audio"]
@@ -253,48 +293,38 @@ class SpecRollDiffusion(pl.LightningModule):
         t = torch.randint(0, self.hparams.timesteps, (batch_size,), device=device).long()
 
         noise = torch.randn_like(roll) # creating label noise
-
-        x_noisy = q_sample( # sampling noise at time t
+        
+        x_t = q_sample( # sampling noise at time t
             x_start=roll,
             t=t,
             sqrt_alphas_cumprod=self.sqrt_alphas_cumprod,
             sqrt_one_minus_alphas_cumprod=self.sqrt_one_minus_alphas_cumprod,
             noise=noise)
         
-        predicted_noise = self(x_noisy, waveform, t) # predict the noise at previous step (t-1)
-        diffusion_loss = self.p_losses(loss_type=self.hparams.loss_type)
-        self.log("Test/loss", loss)
+        epsilon_pred = self(x_t, waveform, t) # predict the noise at previous step (t-1)
+        diffusion_loss = self.p_losses(noise, epsilon_pred, loss_type=self.hparams.loss_type)
         
-        fig_roll_pred, ax_roll_pred = plt.subplots(2,2)
-        fig_roll_label, ax_roll_label = plt.subplots(2,2)
+        pred_roll = extract_x0(
+            x_t,
+            epsilon_pred,
+            t,
+            sqrt_alphas_cumprod=self.sqrt_alphas_cumprod,
+            sqrt_one_minus_alphas_cumprod=self.sqrt_one_minus_alphas_cumprod)
+        pred_roll = torch.sigmoid(pred_roll) # to convert logit into probability
+        amt_loss = F.binary_cross_entropy(pred_roll, roll)
         
-        if batch_idx==0:
-            for idx in range(4): # visualize only 4 piano rolls
-                # roll_pred (1, T, F)
-                ax_roll_pred.flatten()[idx].imshow(roll_pred[idx][0].cpu())
-            self.logger.experiment.add_figure(
-                f"prediction_{idx}",
-                fig_roll_pred)        
+        losses = {
+            "diffusion_loss": diffusion_loss,
+            "amt_loss": amt_loss
+        }
         
-    
-#     def p_losses(self, x_start, waveform, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, noise=None, loss_type="l1"):
-#         if noise is None:
-#             noise = torch.randn_like(x_start)
-
-#         x_noisy = q_sample(x_start=x_start, t=t, sqrt_alphas_cumprod=sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod=sqrt_one_minus_alphas_cumprod, noise=noise)
-#         predicted_noise = self(x_noisy, waveform, t)
-
-#         if loss_type == 'l1':
-#             loss = F.l1_loss(noise, predicted_noise)
-#         elif loss_type == 'l2':
-#             loss = F.mse_loss(noise, predicted_noise)
-#         elif loss_type == "huber":
-#             loss = F.smooth_l1_loss(noise, predicted_noise)
-#         else:
-#             raise NotImplementedError()
-
-#         return loss, predicted_noise
-
+        tensors = {
+            "pred_roll": pred_roll,
+            "label_roll": roll
+        }
+        
+        return losses, tensors    
+        
     def p_losses(self, label, prediction, loss_type="l1"):
         if loss_type == 'l1':
             loss = F.l1_loss(label, prediction)
@@ -305,7 +335,7 @@ class SpecRollDiffusion(pl.LightningModule):
         else:
             raise NotImplementedError()
 
-        return loss, predicted_noise
+        return loss
     
 
     def configure_optimizers(self):
