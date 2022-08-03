fe8c8a61029efa48f34bf5b720b29b8f895fe666
Kin Wai Cheuk <kinwai.cheuk@sony.com>
2022-08-03 15:13:52 +0900

    added predict.py

---
+++ predict.py
@@ -0,0 +1,38 @@
+from tqdm import tqdm
+import hydra
+from hydra.utils import to_absolute_path
+
+import torch
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from torch.optim import Adam
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+import model as Model
+
+from AudioLoader.music.amt import MAPS, MAESTRO
+
+@hydra.main(config_path="config", config_name="test")
+def main(cfg):       
+    cfg.data_root = to_absolute_path(cfg.data_root)
+
+    test_set = MAESTRO(**cfg.dataset.test)
+        
+    test_loader = DataLoader(test_set, batch_size=4)
+
+    # Model
+    model = getattr(Model, cfg.model.name).load_from_checkpoint(to_absolute_path(cfg.checkpoint_path))
+    
+    name = f"Test-{cfg.model.name}-" \
+           f"MAESTRO"
+    logger = TensorBoardLogger(save_dir=".", version=1, name=name)    
+
+    trainer = pl.Trainer(**cfg.trainer,
+                         logger=logger)
+    
+    trainer.predict(model, test_loader)
+    
+if __name__ == "__main__":
+    main()
