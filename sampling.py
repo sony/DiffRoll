added inpainting (known bug, -1 masking does not work)

sampling.py
@@ -12,19 +12,29 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 import model as Model
+import AudioLoader.music.amt as MusicDataset
 
 from AudioLoader.music.amt import MAPS, MAESTRO
+from omegaconf import OmegaConf
 
-@hydra.main(config_path="config", config_name="generate")
+@hydra.main(config_path="config", config_name="sampling")
 def main(cfg):       
     cfg.data_root = to_absolute_path(cfg.data_root)
-    
     S = cfg.dataset.num_samples # choose the number of samples to generate
+    x = torch.randn(S, 1, 640, 88)    
     
-    x = torch.randn(S, 1, 640, 88)
-    waveform = torch.randn(S, 327680)
-    dataset = TensorDataset(x, waveform)
+    if cfg.task.sampling.type=='inpainting_ddpm_x0':
+        dataset = getattr(MusicDataset, cfg.dataset.name)(**OmegaConf.to_container(cfg.dataset.args, resolve=True))
+        waveform = torch.empty(S, cfg.dataset.args.sequence_length)
+        for i in range(S):
+            waveform[i] = dataset[i]['audio']
+        print(f"{waveform.shape=}")
+        dataset = TensorDataset(x, waveform)
         
+    elif cfg.task.sampling.type=='generation_ddpm_x0':
+        waveform = torch.randn(S, 327680)
+        dataset = TensorDataset(x, waveform)
+
     loader = DataLoader(dataset, **cfg.dataloader)
 
     # Model

task/diffusion.py
@@ -963,8 +963,35 @@ class SpecRollDiffusion(pl.LightningModule):
                     x-self.sqrt_alphas_cumprod[t_index]* x0_pred)/self.sqrt_one_minus_alphas_cumprod[t_index]) + (
                 sigma * torch.randn_like(x))
 
-        return model_mean, _               
+        return model_mean, _
     
+    def inpainting_ddpm_x0(self, x, waveform, t_index):
+        # x is x_t, when t=T it is pure Gaussian
+        
+        # boardcasting t_index into a tensor
+        t_tensor = torch.tensor(t_index).repeat(x.shape[0]).to(x.device)
+        
+        # Equation 11 in the paper
+        # Use our model (noise predictor) to predict the mean 
+        x0_pred_c, spec = self(x, waveform, t_tensor, inpainting=self.hparams.inpainting)
+        x0_pred_0, _ = self(x, torch.zeros_like(waveform), t_tensor, sampling=True) # if sampling = True, the input condition will be overwritten
+        x0_pred = (1+self.hparams.sampling.w)*x0_pred_c - self.hparams.sampling.w*x0_pred_0
+#         x0_pred = x0_pred_c
+        # x0_pred = x0_pred_0
+
+        if t_index == 0:
+            sigma = (1/self.sqrt_one_minus_alphas_cumprod[t_index]) * (
+                torch.sqrt(1-self.alphas[t_index]))            
+            model_mean = x0_pred / self.sqrt_alphas_cumprod[t_index] 
+        else:
+            sigma = (self.sqrt_one_minus_alphas_cumprod[t_index-1]/self.sqrt_one_minus_alphas_cumprod[t_index]) * (
+                torch.sqrt(1-self.alphas[t_index]))                    
+            model_mean = (self.sqrt_alphas_cumprod[t_index-1]) * x0_pred + (
+                torch.sqrt(1 - self.sqrt_alphas_cumprod[t_index-1]**2 - sigma**2) * (
+                    x-self.sqrt_alphas_cumprod[t_index]* x0_pred)/self.sqrt_one_minus_alphas_cumprod[t_index]) + (
+                sigma * torch.randn_like(x))
+
+        return model_mean, spec
     
     def cfdg_ddim_x0(self, x, waveform, t_index):
         # x is x_t, when t=T it is pure Gaussian